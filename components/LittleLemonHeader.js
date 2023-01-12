b9d56230442f8c5a78b72a254ce4cf89bce3b39a
Bhumika Shrivastav <shrivastav.bhumika@outlook.com>
2023-01-12 13:52:29 +0100

    Render Large List with SectionList

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -10,7 +10,7 @@ export default function LittleLemonHeader() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 0.1,
+		flex: 0.15,
 		backgroundColor: '#EE9972'
 	},
 	headerText: {
--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,74 +1,113 @@
-import * as React from 'react';
+import React from 'react';
 
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { Menu } from 'react-native-paper';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
 const menuItemsToDisplay = [
-	{ name: 'Hummus', price: '$5.00', id: '1A' },
-	{ name: 'Moutabal', price: '$5.00', id: '2B' },
-	{ name: 'Falafel', price: '$7.50', id: '3C' },
-	{ name: 'Marinated Olives', price: '$5.00', id: '4D' },
-	{ name: 'Kofta', price: '$5.00', id: '5E' },
-	{ name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-	{ name: 'Lentil Burger', price: '$10.00', id: '7G' },
-	{ name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-	{ name: 'Kofta Burger', price: '$11.00', id: '9I' },
-	{ name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-	{ name: 'Fries', price: '$3.00', id: '11K' },
-	{ name: 'Buttered Rice', price: '$3.00', id: '12L' },
-	{ name: 'Bread Sticks', price: '$3.00', id: '13M' },
-	{ name: 'Pita Pocket', price: '$3.00', id: '14N' },
-	{ name: 'Lentil Soup', price: '$3.75', id: '15O' },
-	{ name: 'Greek Salad', price: '$6.00', id: '16Q' },
-	{ name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-	{ name: 'Baklava', price: '$3.00', id: '18S' },
-	{ name: 'Tartufo', price: '$3.00', id: '19T' },
-	{ name: 'Tiramisu', price: '$5.00', id: '20U' },
-	{ name: 'Panna Cotta', price: '$5.00', id: '21V' }
+	{
+		title: 'Appetizers',
+		data: [
+			{ name: 'Hummus', price: '$5.00' },
+			{ name: 'Moutabal', price: '$5.00' },
+			{ name: 'Falafel', price: '$7.50' },
+			{ name: 'Marinated Olives', price: '$5.00' },
+			{ name: 'Kofta', price: '$5.00' },
+			{ name: 'Eggplant Salad', price: '$8.50' }
+		]
+	},
+	{
+		title: 'Main Dishes',
+		data: [
+			{ name: 'Lentil Burger', price: '$10.00' },
+			{ name: 'Smoked Salmon', price: '$14.00' },
+			{ name: 'Kofta Burger', price: '$11.00' },
+			{ name: 'Turkish Kebab', price: '$15.50' }
+		]
+	},
+	{
+		title: 'Sides',
+		data: [
+			{ name: 'Fries', price: '$3.00', id: '11K' },
+			{ name: 'Buttered Rice', price: '$3.00' },
+			{ name: 'Bread Sticks', price: '$3.00' },
+			{ name: 'Pita Pocket', price: '$3.00' },
+			{ name: 'Lentil Soup', price: '$3.75' },
+			{ name: 'Greek Salad', price: '$6.00' },
+			{ name: 'Rice Pilaf', price: '$4.00' }
+		]
+	},
+	{
+		title: 'Desserts',
+		data: [
+			{ name: 'Baklava', price: '$3.00' },
+			{ name: 'Tartufo', price: '$3.00' },
+			{ name: 'Tiramisu', price: '$5.00' },
+			{ name: 'Panna Cotta', price: '$5.00' }
+		]
+	}
 ];
 
-const Item = ({ name, price }) => {
-	console.log('' + name);
-	console.log('' + price);
-	return (
-		<View style={styles.innerContainer}>
-			<Text style={styles.itemText}>{name}</Text>
-			<Text style={styles.itemText}>{price}</Text>
-		</View>
-	);
-};
+const Item = ({ name, price }) => (
+	<View style={menuStyles.innerContainer}>
+		<Text style={menuStyles.itemText}>{name}</Text>
+		<Text style={menuStyles.itemText}>{price}</Text>
+	</View>
+);
+
+const Separator = () => <View style={menuStyles.separator} />;
+
+const Footer = () => <Text style={menuStyles.footerText}>All Rights Reserved by Little Lemon 2022</Text>;
 
 const MenuItems = () => {
 	const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
+	const renderSectionHeader = ({ section: { title } }) => <Text style={menuStyles.sectionHeader}>{title} </Text>;
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.headerText}>View Menu</Text>
-			<FlatList data={menuItemsToDisplay} keyExtractor={(item) => item.id} renderItem={renderItem} />
+		<View style={menuStyles.container}>
+			<SectionList
+				keyExtractor={(item, index) => item + index}
+				sections={menuItemsToDisplay}
+				renderItem={renderItem}
+				renderSectionHeader={renderSectionHeader}
+				//ListFooterComponent={Footer}
+				//ItemSeparatorComponent={Separator}
+			/>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({
+// Add styles to the component
+const menuStyles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 0.95
 	},
 	innerContainer: {
 		paddingHorizontal: 40,
 		paddingVertical: 20,
-		backgroundColor: 'black',
+		backgroundColor: '#333333',
 		flexDirection: 'row',
 		justifyContent: 'space-between'
 	},
-	headerText: {
-		color: ' white',
-		fontSize: 20,
+	sectionHeader: {
+		backgroundColor: '#F4CE14',
+		color: '#333333',
+		fontSize: 25,
 		flexWrap: 'wrap',
 		textAlign: 'center'
 	},
 	itemText: {
 		color: '#F4CE14',
-		fontSize: 15
+		fontSize: 20
+	},
+	separator: {
+		borderBottomWidth: 1,
+		borderColor: '#EDEFEE'
+	},
+	footerText: {
+		color: '#EDEFEE',
+		fontSize: 20,
+		flexWrap: 'wrap',
+		textAlign: 'center'
 	}
 });
 
